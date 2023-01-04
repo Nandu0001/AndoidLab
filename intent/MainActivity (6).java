67ff5858400f121e28a324ffae8bfdd15bb92dd2
Nandana MK <96459268+Nandu0001@users.noreply.github.com>
2023-01-04 19:03:41 +0530

    finish

--- intent/MainActivity (6).java
+++ intent/MainActivity (6).java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn=(Button) findViewById(R.id.btn);
+        Button btn=(Button)findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
